3f36e4ed4cbd747df98eaffee5af147587d2a73e
Artem <artglad05@mail.ru>
2023-11-12 23:51:07 +0500

    add new routs for add || remove affair

--- backend/index.py
+++ backend/index.py
@@ -1,15 +1,23 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import json
+import socket
 
+host = socket.gethostname()
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route('/')
+
 def hello_world():
     print('server run')
     return 'Hello World!'
 
+
 @app.route('/api')
+
 def api():
     data = [{'message': 'Request successful'}]
 
@@ -20,17 +28,52 @@ def api():
 
     return data
 
+
+@app.route('/getAffairs', methods=['GET'])
+
+def get_user_affairs():
+
+    with open('backend/user_affairs.json', 'r', encoding='utf-8') as file:
+        user_affairs = json.load(file)
+    print(user_affairs)
+    return jsonify(user_affairs)  # Отправляем ответ клиенту в формате JSON
+
+
 @app.route('/addAffair', methods=['POST'])
+
 def add_affair():
-
-    print(request)
     new_affair = request.get_json()  # Получаем данные из тела запроса
-    
 
-    with open('affairs.json', 'w') as file:
-        json.dump(new_affair, file)
+    with open('backend/user_affairs.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        data['users'][0]['affairs'].append(new_affair)  # Добавляем новый элемент в список affairs
+
+    with open('backend/user_affairs.json', 'w', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)  # Сохраняем обновленные данные в файл JSON
 
     return jsonify({'message': 'New affair added successfully'})  # Отправляем ответ клиенту
 
+
+@app.route('/removeAffair', methods=['POST'])
+
+def remove_affair():
+    id_affair = (request.get_json())["id"]  # Получаем данные из тела запроса
+    print(id_affair)
+    count = 0
+    with open('backend/user_affairs.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        affairs = data['users'][0]['affairs']  # удаляем новый элемент из списока affairs
+        for affair in affairs:
+            if affair["id"] == id_affair:
+                affairs.pop(count)
+            count += 1
+
+    with open('backend/user_affairs.json', 'w', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)  # Сохраняем обновленные данные в файл JSON
+
+    return jsonify({'message': 'New affair added successfully'})  # Отправляем ответ клиенту
+
+
+    
 if __name__ == '__main__':
-    app.run(debug='True', ssl_context='adhoc')
+    app.run( host=host, debug='True', ssl_context=('cert.pem', 'key.pem'))
