e491a18f384cc6acb4f4f837079fc524e678f80f
Artem <artglad05@mail.ru>
2023-11-17 00:49:25 +0500

    updated data storage and routes

--- backend/index.py
+++ backend/index.py
@@ -10,14 +10,12 @@ CORS(app)
 
 
 @app.route('/')
-
 def hello_world():
     print('server run')
     return 'Hello World!'
 
 
 @app.route('/api')
-
 def api():
     data = [{'message': 'Request successful'}]
 
@@ -30,24 +28,41 @@ def api():
 
 
 @app.route('/getAffairs', methods=['GET'])
-
 def get_user_affairs():
+    user_id = request.args.get('userId')
 
     with open('backend/user_affairs.json', 'r', encoding='utf-8') as file:
         user_affairs = json.load(file)
-    print(user_affairs)
-    return jsonify(user_affairs)  # Отправляем ответ клиенту в формате JSON
+
+    if user_id not in user_affairs:
+        user_affairs[user_id] = {'affairs':{}}
+
+    print(user_affairs[user_id])
+    date = request.args.get('date')
+    # print('Обращение в affairs' + user_affairs[user_id]['affairs'])
+    if date not in user_affairs[user_id]['affairs']:
+        user_affairs[user_id]['affairs'][date] = []
+
+    with open('backend/user_affairs.json', 'w', encoding='utf-8') as file:
+        json.dump(user_affairs, file, ensure_ascii=False, indent=4)
+
+    return jsonify(user_affairs[user_id])  # Отправляем ответ клиенту в формате JSON.
 
 
 @app.route('/addAffair', methods=['POST'])
-
 def add_affair():
     new_affair = request.get_json()  # Получаем данные из тела запроса
+    user_id = request.args.get('userId')
 
+    date = request.args.get('date')
     with open('backend/user_affairs.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
-        data['users'][0]['affairs'].append(new_affair)  # Добавляем новый элемент в список affairs
 
+    if date in data[user_id]['affairs']:
+        data[user_id]['affairs'][date].append(new_affair)  # Добавляем новый элемент в список affairs
+    else:
+        data[user_id]['affairs'] = {date:[new_affair]}
+        print(data[user_id]['affairs'])
     with open('backend/user_affairs.json', 'w', encoding='utf-8') as file:
         json.dump(data, file, ensure_ascii=False, indent=4)  # Сохраняем обновленные данные в файл JSON
 
@@ -55,24 +70,22 @@ def add_affair():
 
 
 @app.route('/removeAffair', methods=['POST'])
-
 def remove_affair():
+    user_id = request.args.get('userId')
     id_affair = (request.get_json())["id"]  # Получаем данные из тела запроса
     print(id_affair)
-    count = 0
     with open('backend/user_affairs.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
-        affairs = data['users'][0]['affairs']  # удаляем новый элемент из списока affairs
-        for affair in affairs:
-            if affair["id"] == id_affair:
-                affairs.pop(count)
-            count += 1
+        user_affairs = data.get(str(user_id), {}).get('affairs', {})
+        for date, affairs in user_affairs.items():
+            for affair in affairs:
+                if affair["id"] == id_affair:
+                    affairs.remove(affair)
 
     with open('backend/user_affairs.json', 'w', encoding='utf-8') as file:
         json.dump(data, file, ensure_ascii=False, indent=4)  # Сохраняем обновленные данные в файл JSON
 
-    return jsonify({'message': 'New affair added successfully'})  # Отправляем ответ клиенту
-
+    return jsonify({'message': 'Affair removed successfully'})  # Отправляем ответ клиенту
 
     
 if __name__ == '__main__':
