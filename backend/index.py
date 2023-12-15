6ed0c9beb14b8ec527a6a4b21243f1f3ecc11f60
Artem <artglad05@mail.ru>
2023-12-15 23:19:23 +0500

    2

--- backend/index.py
+++ backend/index.py
@@ -227,6 +227,6 @@ def remove_affair():
     
 if __name__ == '__main__':
     try:
-        app.run( host=host, debug='True', ssl_context=('cert.pem', 'Ubuntu-STD2-1-1-10GB-iCsllaiF.pem'))
+        app.run( host=host, port='8078', debug='True', ssl_context=('cert.pem', 'Ubuntu-STD2-1-1-10GB-iCsllaiF.pem'))
     except:
-        app.run( host=host, debug='True', ssl_context=('cert.pem', 'key.pem'))
+        app.run( host=host, port='8078', debug='True', ssl_context=('cert.pem', 'key.pem'))
