3173250c3f8a0e1892d32af4bdf6c77f096b02ab
Artem <artglad05@mail.ru>
2023-12-16 14:35:20 +0500

    host

--- backend/app.py
+++ backend/app.py
@@ -226,4 +226,4 @@ def remove_affair():
 
     
 if __name__ == '__main__':
-    app.run( debug='True', ssl_context=('cert.pem', 'key.pem'))
+    app.run( host=host, debug='True', ssl_context=('cert.pem', 'key.pem'))
